6a7efacb551afda9fa28aaa0207b73ca4ef3de86
cloudaipro <cloudaipro@gmail.com>
2023-05-17 07:44:54 -0700

    Initial Commit

---
+++ shadow_remover/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  shadow_remover
+//
+//  Created by alex on 2023-05-17.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
